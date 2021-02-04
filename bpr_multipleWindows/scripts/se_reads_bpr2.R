949afd9187bd37ea327a7a56ed290e33e115fcdd
zhamadeh <zeid.hamadeh@hotmail.com>
2021-02-04 11:59:23 -0800

    final window sizes, crashed earlier because of space

--- bpr_multipleWindows/scripts/se_reads_bpr2.R
+++ bpr_multipleWindows/scripts/se_reads_bpr2.R
@@ -1,7 +1,7 @@
 BiocManager::install("breakpointR")
 library(breakpointR)
 
-for (window_size in c(800,1600)){
+for (window_size in c(1600)){
 	breakpointR::breakpointr(inputfolder = "se_alignment_pipeline/mdup",outputfolder = paste0("se_alignment_pipeline/bpr_MultipleWind_",window_size,"reads/"),
 							 pairedEndReads=FALSE, numCPU=8,windowsize=window_size,binMethod="reads",peakTh=0.3875,
 							 min.mapq=7.75,trim=6.5,background=0.15)
